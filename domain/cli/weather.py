92225c65061c61637c089f9761422f1c9870b0c9
Translation-regulator <changchaocheng870405@gmail.com>
2026-06-09 00:01:11 +0800

    fix: miss cli excute entry site

--- domain/cli/weather.py
+++ domain/cli/weather.py
@@ -3,6 +3,7 @@ from domain.weather.pipelines.general_36hour.transform import transform as trans
 from domain.weather.pipelines.three_days.update import update as update_three_day
 from domain.weather.pipelines.three_days.transform import transform as transform_three_day
 from domain.weather.pipelines.one_week.update import update as update_one_week
+from domain.weather.pipelines.one_week.transform import transform as transform_one_week
 
 def run():
     update_36()
@@ -10,6 +11,7 @@ def run():
     update_three_day()
     transform_three_day()
     update_one_week()
+    transform_one_week()
     
 if __name__ == "__main__":
     run()
