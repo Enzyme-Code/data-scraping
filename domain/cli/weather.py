f7e1d5b850a488561c8c266e9e275b413ee9b3a6
Translation-regulator <changchaocheng870405@gmail.com>
2026-06-08 23:45:04 +0800

    feat: create cli for update weather information

---
+++ domain/cli/weather.py
@@ -0,0 +1,15 @@
+from domain.weather.pipelines.general_36hour.update import update as update_36
+from domain.weather.pipelines.general_36hour.transform import transform as transform_36
+from domain.weather.pipelines.three_days.update import update as update_three_day
+from domain.weather.pipelines.three_days.transform import transform as transform_three_day
+from domain.weather.pipelines.one_week.update import update as update_one_week
+
+def run():
+    update_36()
+    transform_36() 
+    update_three_day()
+    transform_three_day()
+    update_one_week()
+    
+if __name__ == "__main__":
+    run()
