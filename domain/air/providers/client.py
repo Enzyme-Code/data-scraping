13c572a41947b738f522dde0c0863e8edfef6e13
Translation-regulator <changchaocheng870405@gmail.com>
2026-08-09 12:53:32 +0800

    core: keep client clear

--- domain/air/providers/client.py
+++ domain/air/providers/client.py
@@ -1,8 +1,5 @@
 from .base import AirBase
 
-import os
-from dotenv import load_dotenv
-
 class AirClient(AirBase):
 
     def get_air_data(self, data_id: str , limit: int = None, offset: int = None):
